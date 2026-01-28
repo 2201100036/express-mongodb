80e3c5df9d1a494b620e4a0251dd56c69e4b23f1
2201100036 <2201100036gc@cyber-u.ac.jp>
2026-01-29 05:33:33 +0900

    課題提出用：バックエンドとフロントエンド完成

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-mongoose.connect("mongodb+srv://matsunami2:banana1208@test.lfwohhl.mongodb.net/?retryWrites=true&w=majority"
-).then(() => console.log("データベース接続に成功しました"))
+app.use(cors());
+
+// 接続文字列の .net/ と ? の間に "notes" を追加しました
+mongoose.connect("mongodb+srv://2201100036:natuyasumi5176@cluster0.kp1ld0r.mongodb.net/notes?appName=Cluster0")
+.then(() => console.log("データベース接続に成功しました"))
 .catch((err) => console.log(err));
 
+app.get('/notes_from_b', async (req, res) => {
+    try {
+        // notesデータベース内の notesコレクションからデータを取得
+        const notes = await mongoose.connection.db.collection('notes').find().toArray();
+        res.json(notes);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
 app.listen(3000, () => {
-console.log("サーバが起動しました。")
-})
+    console.log("サーバが起動しました。http://localhost:3000/notes_from_b を開いてください");
+});

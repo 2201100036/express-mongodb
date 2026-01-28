15347281a5d7ce0bf8649b321e727ee5baf96d26
2201100036 <2201100036gc@cyber-u.ac.jp>
2026-01-29 06:09:11 +0900

    静的ファイル配信の設定を追加

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 
 app.use(cors());
 
-// 接続文字列の .net/ と ? の間に "notes" を追加しました
 mongoose.connect("mongodb+srv://2201100036:natuyasumi5176@cluster0.kp1ld0r.mongodb.net/notes?appName=Cluster0")
 .then(() => console.log("データベース接続に成功しました"))
 .catch((err) => console.log(err));
 
+app.use(express.static(__dirname));
+
 app.get('/notes_from_b', async (req, res) => {
     try {
-        // notesデータベース内の notesコレクションからデータを取得
         const notes = await mongoose.connection.db.collection('notes').find().toArray();
         res.json(notes);
     } catch (err) {

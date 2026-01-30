31ad0e7b9cb6cb3fe8ea87a8b8d150151b59193f
2201100036 <2201100036gc@cyber-u.ac.jp>
2026-01-30 14:02:07 +0900

    修正

--- app.js
+++ app.js
@@ -3,19 +3,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-
 const PORT = process.env.PORT || 3000;
-
 
 app.use(cors());
 app.use(express.static(__dirname)); 
 
-
 const notesRouter = require('./routes/notes_from_b');
-
-
 app.use('/notes_from_b', notesRouter);
 
 app.listen(PORT, () => {
-    console.log(`サーバが起動しました。ポート番号: ${PORT}`);
+    console.log(`サーバが起動しました。`);
 });
